d88cf4d42170bfe1797251446cc9b273aef41899
liangzhaoyuan12 <liangzhaoyuan12@outlook.com>
2025-03-07 13:24:53 +0800

    1.0，增加抛出异常throw

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,18 @@ int a(int b,int c)
 {
     return b/c;
 }
+int d()
+{
+    throw std::runtime_error("runtime error");
+    //异常抛出
+}
 /**
  * @brief 
  * 异常处理的简单例子
  * 异常处理不适合所有异常
  * 所以这个例子只是演示如何使用异常处理
  * 分母为0并不会走异常处理
+ * 但是如果手动抛出异常，那么将会走异常处理
  * @param argc 
  * @param argv 
  * @return int 
@@ -19,6 +25,7 @@ int main(int argc, char *argv[])
     try
     {
         cout<<a(1,0);
+        d();
     }
     catch(const std::exception& e)
     {
